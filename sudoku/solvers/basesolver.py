026511cc391e24dffbc80a522a1e226fd61e25e7
Douglas Finlay <douglas@douglasfinlay.com>
2020-04-13 13:30:00 +0100

    Added abstract solver class

---
+++ sudoku/solvers/basesolver.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class BaseSolver(ABC):
+    grid = None
+
+    steps = 0
+
+    def __init__(self, grid):
+        self.grid = grid
+
+    def solve(self) -> bool:
+        """
+        Attempts to solve the sudoku puzzle.
+
+        :returns: True if successful, otherwise False
+        """
+        while not self.solved:
+            success = self.step()
+            if not success:
+                return False
+        return True
+
+    @abstractmethod
+    def step(self):
+        """
+        Runs a single iteration of the solving algorithm.
+
+        :returns: False if there are no more steps available to take, otherwise True
+        """
+        self.steps += 1
